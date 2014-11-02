a01ae0d0e45649e704582d413c4f21ead454bf3c
robertwenquan <robert.wen@nyu.edu>
2014-11-01 22:02:51 -0400

    can detect dup add test case and structure

---
+++ programming-languages/programming-assignment2/verify-case.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+BIN="./assignment2"
+
+cnt=0
+succ=0
+fail=0
+for case in $(ls -1 testcases/case-*.input)
+do
+  #filename="${case##*/}"
+  basename="${case%.*}"
+
+  cnt=$((cnt+1))
+
+  output="${basename}.output"
+  if [ -e "${output}" ]
+  then
+    $BIN < $case | diff -u ${output} -
+    if [ $? -eq 0 ]
+    then
+      succ=$((succ+1))
+    else
+      fail=$((fail+1))
+    fi
+  else
+    echo "<<< WARNING: No sample output file for $case !!!"
+  fi
+done
+
+rate=$(echo "scale=4;$succ/$cnt*100" | bc)
+echo
+echo
+printf "Success [%d/%d], in %.2f%%\n" $succ $cnt $rate
