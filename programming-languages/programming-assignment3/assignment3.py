4411013d4ae8de76535273819061c02c2935dedc
robertwenquan <robert.wen@nyu.edu>
2014-11-11 22:29:04 -0500

    initial version of the haskell basic function initial version of the python version and the git attribute file

---
+++ programming-languages/programming-assignment3/assignment3.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import string
+
+
+def F(n):
+  if(n==0 or n==1 or n==2 or n==3):
+    return 1
+
+  result = int((F(n-1)+F(n-2))*F(n-3)/F(n-4))
+  return result
+
+
+def get_num(n):
+  print F(n)
+
+
+def sum_num(n):
+  if n == 0:
+    return 1
+  else:
+    return F(n) + sum_num(n-1)
+
+
+def get_sum(n):
+  print sum_num(n)
+
+
+def get_bounds(n):
+  print lower_bound(n)
+  print upper_bound(n)
+
+
+def upper_bound(n):
+  i = 0
+
+  while n >= F(i):
+    i += 1
+
+  return F(i)
+
+
+def lower_bound(n):
+  i = 0
+
+  while F(i+1) < n:
+    i += 1
+
+  return F(i)
+
+
+f_in  = open('/proc/self/fd/0', 'r')
+f_out = open('/proc/self/fd/1', 'w')
+
+
+# TODO: handle EOF
+while True:
+  line = string.replace(f_in.readline(), "\n", "")
+  if (line == ""):
+    print "ERR"
+    break
+
+  cmd_arr = string.split(line, " ")
+  if len(cmd_arr) > 2:
+    print "ERR"
+    break
+
+  CMD = cmd_arr[0]
+
+  if (line == "QUIT"):
+    break
+
+  ARG = int(cmd_arr[1])
+
+  if (CMD == "NTH"):
+    get_num(ARG)
+  elif (CMD == "SUM"):
+    get_sum(ARG)
+  elif (CMD == "BOUNDS"):
+    get_bounds(ARG)
+  else:
+    print "ERR"
+    break
+
--- programming-languages/programming-assignment3/qw476.hs
+++ programming-languages/programming-assignment3/qw476.hs
@@ -1,1 +1,75 @@
-main = putStrLn "Hello, World!"
+------------------------------------------------------------------------
+---- Programming Language Class Fall 2014 Semester
+----   Programming Assignment 3
+----
+----   Quan Wen (robert.wen@nyu.edu) | netid: qw476
+----
+----   $Id$
+------------------------------------------------------------------------
+
+
+--
+-- Define the f function
+--
+f :: Integer -> Integer
+f 0 = 1
+f 1 = 1
+f 2 = 1
+f 3 = 1
+f n = (f (n-1) + f (n-2)) * f (n-3) `div` f (n-4)
+
+
+--
+-- Define the sumf function
+--
+sumf ::  Integer -> Integer
+sumf 0 = 1
+sumf n = f (n) + sumf (n-1)
+
+
+--
+-- Define the lower bound function
+--
+lower_bound :: Integer -> Integer
+lower_bound n = get_lower 0
+  where
+  get_lower :: Integer -> Integer
+  get_lower x = if f (x+1) >= n
+                then
+                  f x
+                else
+                  get_lower (x+1)
+
+--
+-- Define the higher bound function
+--
+upper_bound :: Integer -> Integer
+upper_bound n = get_upper 0
+  where
+  get_upper :: Integer -> Integer
+  get_upper x = if n < f x
+                then
+                  f x
+                else
+                  get_upper (x+1)
+
+
+main = do
+
+-------- Input and Output Examples --------
+--  putStrLn("wwwwww")
+--  name <- getLine
+--  putStrLn("Hello, World " ++ name ++ "!")
+-------------------------------------------
+
+  let x = 7
+  let y = f 4
+  let z1 = sumf 4
+  let z2 = lower_bound 16
+  let z3 = upper_bound 16
+
+  print y
+  print z1
+  print z2
+  print z3
+
