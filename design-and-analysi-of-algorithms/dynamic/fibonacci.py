5a6db5f1ae039955a47b0a5fe41a96fab41cb37b
robertwenquan <robert.wen@nyu.edu>
2014-11-25 21:01:29 -0500

    add two DP examples

--- design-and-analysi-of-algorithms/dynamic/fibonacci.py
+++ design-and-analysi-of-algorithms/dynamic/fibonacci.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
+#TODO: save the result during the traditional one, using top-down memorization
+
 def fib_stupid(n):
   if n == 1 or n == 2:
     return 1
   else:
     return fib_stupid(n-1) + fib_stupid(n-2)
 
+# 2nd approach, using bottom-up
 def fib_bottom_up(n):
   fib = dict()
 
@@ -18,5 +21,5 @@ def fib_bottom_up(n):
 
   return fib[n-1] + fib[n-2]
 
-#print fib_stupid(40)
+print fib_stupid(30)
 print fib_bottom_up(100)
--- design-and-analysi-of-algorithms/dynamic/rod_cutting.py
+++ design-and-analysi-of-algorithms/dynamic/rod_cutting.py
@@ -1,2 +1,77 @@
 #!/usr/bin/python
 
+import timeit
+
+# straight-forward implementation
+
+def generate_price(n):
+  price_list = dict()
+
+  unit_price = 1
+
+  for i in range(1,n+1):
+    price_list[i] = unit_price * i
+
+  return price_list
+
+def generate_price2(n):
+  price_list = dict()
+
+  price_list[0] = 0
+  price_list[1] = 1
+  price_list[2] = 5
+  price_list[3] = 8
+  price_list[4] = 10
+  price_list[5] = 13
+
+  return price_list
+
+def rod_cutting0(n, PRICE):
+
+  if n == 0:
+    return 0
+
+  result = PRICE[1] + rod_cutting0(n-1, PRICE)
+
+  for i in range(2,n+1):
+    current = PRICE[i] + rod_cutting0(n-i, PRICE)
+    if (current > result):
+      result = current
+
+  return result
+
+# top-down memorization method
+
+def memo_rod_cutting(n, PRICE):
+  CACHE = dict()
+  for i in range(0,n+1):
+    CACHE[i] = -1
+
+  return memo_rod_cutting_aux(n, PRICE, CACHE)
+
+
+def memo_rod_cutting_aux(n, PRICE, CACHE):
+  if (CACHE[n] != -1):
+    return CACHE[n]
+
+  if (n == 0):
+    CACHE[n] = 0
+    return 0
+
+  result = PRICE[1] + memo_rod_cutting_aux(n-1, PRICE, CACHE)
+  for i in range(2,n+1):
+    current = PRICE[i] + rod_cutting0(n-i, PRICE)
+    if (current > result):
+      result = current
+
+  CACHE[n] = result
+  return result
+
+
+n = 5
+ROD_PRICE = generate_price2(n)
+#timeit.timeit(rod_cutting0(n, ROD_PRICE), number=1)
+#timeit.timeit(memo_rod_cutting(n, ROD_PRICE), number=1)
+print memo_rod_cutting(n, ROD_PRICE)
+
+
