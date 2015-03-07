6d1d1888ae5e7e14de0dde62ca5c205c9362c4cb
Robert Wen <robert.wen@nyu.edu>
2015-03-07 11:51:12 -0500

    update

--- big-data-analytics/map-reduce/binning/WordCountBinning.java
+++ big-data-analytics/map-reduce/binning/WordCountBinning.java
@@ -1,3 +1,21 @@
+/*
+ * WordCount Map-Reduce example with binning
+ * binning strategy is based on the magnitude of the wordcount
+ *  # of occurrences 1-10 will be stored under result/10
+ *  # of occurrences 11-100 will be stored under result/100
+ *  similarly 1000, 10000, 100000, 1000000 will be there
+ *  utilimately if occurrences exceeds 1 million,
+ *  the results will be put into a directory named "toomany"
+ *
+ * Big Data Analytics, Hadoop Binning assignment
+ * Due 03/07/2015
+ *
+ * Robert Wen (robert.wen@nyu.edu)
+ * NetID: qw476
+ * N12246277
+ *
+ */
+
 import java.io.IOException;
 import java.util.*;
 
@@ -14,6 +32,7 @@ public class WordCountBinning {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
+    /* map class, just like the wordcount */
     public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
       String line = value.toString();
       StringTokenizer tokenizer = new StringTokenizer(line);
@@ -25,6 +44,7 @@ public class WordCountBinning {
   }
 
   public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+    /* reduce class, just like the wordcount */
     public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
       int sum = 0;
       while (values.hasNext()) {
@@ -34,12 +54,28 @@ public class WordCountBinning {
     }
   }
 
-  public static class KeyBasedMultipleTextOutputFormat extends MultipleTextOutputFormat<Text, IntWritable>
+  public static class OrganizeByCountNumber extends MultipleTextOutputFormat<Text, IntWritable>
   {
     @Override
     protected String generateFileNameForKeyValue(Text key, IntWritable value, String name)
     {
-      return key.toString() + "/" + name;
+      int wordcount = value.get();
+
+      if (wordcount <= 10) {
+        return "10/" + name;
+      } else if (wordcount <= 100) {
+        return "100/" + name;
+      } else if (wordcount <= 1000) {
+        return "1000/" + name;
+      } else if (wordcount <= 10000) {
+        return "10000/" + name;
+      } else if (wordcount <= 100000) {
+        return "100000/" + name;
+      } else if (wordcount <= 1000000) {
+        return "1000000/" + name;
+      } else {
+        return "toomany" + name;
+      }
     }
   }
 
@@ -55,7 +91,9 @@ public class WordCountBinning {
     conf.setReducerClass(Reduce.class);
 
     conf.setInputFormat(TextInputFormat.class);
-    conf.setOutputFormat(KeyBasedMultipleTextOutputFormat.class);
+
+    /* override the default TextOutputFormat class with self-defined class */
+    conf.setOutputFormat(OrganizeByCountNumber.class);
 
     FileInputFormat.setInputPaths(conf, new Path(args[0]));
     FileOutputFormat.setOutputPath(conf, new Path(args[1]));
