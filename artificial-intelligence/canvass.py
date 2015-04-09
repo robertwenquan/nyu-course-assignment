d2be2dccfcd7c564ba9b79930130e39b4d69669b
robertwenquan <robert.wen@nyu.edu>
2015-04-09 01:33:23 -0400

    the canvass looks somewhat real

--- artificial-intelligence/canvass.py
+++ artificial-intelligence/canvass.py
@@ -7,7 +7,6 @@
 
 from pprint import pprint
 from Tkinter import *
-import tkMessageBox
 
 class Cell():
   '''
@@ -17,7 +16,7 @@ class Cell():
   y = 0
   status = 'disabled'
 
-  def init(self, x, y, status):
+  def __init__(self, x, y, status):
     self.x = x
     self.y = y
     self.status = status
@@ -41,34 +40,85 @@ class PlayGround():
   unit = 50
   margin = 20
 
-  def __init__(self, ncol = 8, nrow = 14):
+  player = None
+
+  canvass = dict()
+
+  def __init__(self, player, ncol = 8, nrow = 14):
+
     self.width = self.margin * 2 + self.unit * ncol
     self.height = self.margin * 2 + self.unit * nrow
+    self.player = player
+
+    self.init_cells(ncol, nrow)
 
     self.draw_the_playground(ncol, nrow)
+
+  def init_cells(self, ncol, nrow):
+    
+    for i in range(nrow):
+      for j in range(ncol):
+        n = ncol*i + j
+
+        cell = Cell(i,j,'free')
+        self.canvass[n] = {'cell' : cell}
+
+    for x,y in [ (0,0),  (0,1),  (0,2),  (0,5),  (0,6),  (0,7), \
+                 (1,0),  (1,1),                  (1,6),  (1,7), \
+                 (2,0),                                  (2,7), \
+                (11,0),                                 (11,7), \
+                (12,0), (12,1),                 (12,6), (12,7), \
+                (13,0), (13,1), (13,2), (13,5), (13,6), (13,7)]:
+      n = ncol*x + y
+      self.canvass[n]['cell'].status = 'disabled'
+
+    for x,y in [(4,2), (4,3), (4,4), (4,5), \
+                       (5,3), (5,4)]:
+      n = ncol*x + y
+      self.canvass[n]['cell'].status = 'play_bot'
+
+    for x,y in [       (8,3), (8,4), \
+                (9,2), (9,3), (9,4), (9,5)]:
+      n = ncol*x + y
+      self.canvass[n]['cell'].status = 'play_human'
 
   def draw_the_playground(self, ncol, nrow):
 
     top = Tk()
+    top.title('caicai AI programming assignment')
 
     w = Canvas(top, bg="white", height = self.height, width = self.width)
     w.pack()
 
     icon=PhotoImage(file="box.gif")
-    Buttons = dict()
 
-    for i in range(ncol):
-      for j in range(nrow):
-        n = 10*i + j
+    for i in range(nrow):
+      for j in range(ncol):
+        n = ncol*i + j
 
-        Buttons[n] = Button(top, activebackground = 'white', height = 50, width = 50, image = icon, text = str(n), command = helloCallBack)
-        x = 20 + 50 * i
-        y = 20 + 50 * j
-        Buttons[n].place(x=x, y=y)
+        cell = self.canvass[n]['cell']
+
+        if cell.status == 'disabled':
+          color = 'grey'
+        elif cell.status == 'free':
+          color = 'white'
+        elif cell.status == 'play_bot':
+          color = 'blue'
+        elif cell.status == 'play_human':
+          color = 'purple'
+
+        #button = Button(top, activebackground = 'white', height = 50, width = 50, image = icon, background = color, command = self.player.on_click(cell))
+        if color == 'grey':
+          button = Button(top, state = DISABLED, activebackground = 'white', height = 50, width = 50, background = color)
+        else:
+          button = Button(top, activebackground = 'white', height = 50, width = 50, cursor = "target", background = color, command = self.player.on_click(cell))
+
+        x = 20 + 50 * j
+        y = 20 + 50 * i
+        button.place(x=x, y=y, width=self.unit, height=self.unit)
+
+        self.canvass[n]['button'] = button
 
     top.mainloop()
 
-
-def helloCallBack():
-  tkMessageBox.showinfo( "Hello Python", "Hello World")
 
--- artificial-intelligence/playcc.py
+++ artificial-intelligence/playcc.py
@@ -1,12 +1,64 @@
 #!/usr/bin/python
 #
-# playground.py
+# playcc.py
 #
-# this is the playground canvass for the game
+# this is the AI game
 #
 
 from pprint import pprint
 from canvass import PlayGround
 
-PG = PlayGround()
+class Player():
+  '''
+  the AI game player
+  '''
+
+  # name of the player
+  # default is 'aibot'
+  name = 'robot'
+
+  # intelligence level
+  #  1 - low
+  #  2 - medium
+  #  3 - superhigh
+  intell_level = 3
+
+  def __init__(self, name = 'aibot'):
+
+    if name == 'caicai':
+      self.intell_level = 3
+    elif name == 'tao1':
+      self.intell_level = 2
+    elif name == 'tao2':
+      self.intell_level = 1
+
+  def whats_next(self, canvass):
+    '''
+    from current situation, calculate what is next step
+    input:  The Canvass
+    output: ((x1,y1),(x2,y2))
+            (x,y) cooridinate of the source cell
+            (x,y) cooridinate of the detination cell
+    '''
+
+    x1 = 1
+    y1 = 1
+    x2 = 1
+    y2 = 1
+
+    return ((x1,y1),(x2,y2))
+
+  def on_click(self, cell):
+    print "click cell(%d,%d)" % (cell.x, cell.y)
+
+
+#
+# main game starts HERE
+#
+
+# setup the player
+ai_player = Player('caicai')
+
+# kick off the play ground
+PG = PlayGround(player = ai_player)
 
