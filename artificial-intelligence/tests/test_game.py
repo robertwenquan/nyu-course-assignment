3cc8c6d590508188f680b037ce61093166efd73f
robertwenquan <robert.wen@nyu.edu>
2015-04-20 01:02:45 -0400

    add test for game

---
+++ artificial-intelligence/tests/test_game.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#
+# This is a sample unittest file for GameEngine class
+#
+
+import unittest
+from mock import *
+
+from playcc import GameEngine
+
+class TestGameCanvass(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def test_game_init(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
+
