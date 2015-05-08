90347084994dc9882372b7daf0bad7cbe3d8718b
robertwenquan <robert.wen@nyu.edu>
2015-05-07 21:54:05 -0400

    draw game canvass

--- big-data-analytics/cache-camelot-final/draw_canvass.R
+++ big-data-analytics/cache-camelot-final/draw_canvass.R
@@ -16,7 +16,18 @@ plot(c(0,600), c(0,600))
 # black cells are here
 
 # draw all cells
-for (x in 0:13)
-  for (y in 0:7)
-    rect(20 + y * 40, 20 + x * 40, 60 + y * 40, 60 + x * 40, col=5)
+for (x in 0:13) {
+  for (y in 0:7) {
+    if ((y == 0 | y == 1 | y == 2 | y == 5 | y == 6 | y == 7) & (x == 0 | x == 13)) {
+      color <- 'grey'
+    } else if ((y == 0 | y == 1 | y == 6 | y == 7) & (x == 1 | x == 12)) {
+      color <- 'grey'
+    } else if ((y == 0 | y == 7) & (x == 2 | x == 11)) {
+      color <- 'grey'
+    } else {
+      color <- 5
+    }
+    rect(20 + y * 40, 20 + x * 40, 60 + y * 40, 60 + x * 40, col=color)
+  }
+}
 
