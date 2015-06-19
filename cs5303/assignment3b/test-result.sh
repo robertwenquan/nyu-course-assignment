03c59f039f54c1e46e59a9840fac2f14c4e733ad
Robert Wen <robert.wen@nyu.edu>
2015-06-19 11:42:01 -0400

    add raw output from everyone

--- cs5303/assignment3b/test-result.sh
+++ cs5303/assignment3b/test-result.sh
@@ -9,9 +9,21 @@ get_list_netid()
 for netid in $(get_list_netid)
 do
   echo "Checking $netid"
-  ./$netid | sed -e '/^ *$/d' -e 's/^ *//' -e 's/[ \t][ \t]*/\t/g' -e 's/[ \t]*$//' | tr 'a-z' 'A-Z' > results/file1
-  cat results/standard.txt | sed -e '/^ *$/d' -e 's/^ *//' -e 's/[ \t][ \t]*/\t/g' -e 's/[ \t]*$//' | tr 'a-z' 'A-Z' > results/file2
+  ./$netid > results/${netid}.log
+  if [[ "$OSTYPE" == "darwin"* ]]
+  then
+    sed -e '/^ *$/d' -e 's/^ *//' -e 's/[ 	][ 	]*/	/g' -e 's/[ 	]*$//' results/${netid}.log | tr 'a-z' 'A-Z' > results/file1
+    sed -e '/^ *$/d' -e 's/^ *//' -e 's/[ 	][ 	]*/	/g' -e 's/[ 	]*$//' results/standard.txt | tr 'a-z' 'A-Z' > results/file2
+  elif [[ "$OSTYPE" == "linux-gnu" ]]
+  then
+    sed -e '/^ *$/d' -e 's/^ *//' -e 's/[ \t][ \t]*/\t/g' -e 's/[ \t]*$//' results/${netid}.log | tr 'a-z' 'A-Z' > results/file1
+    sed -e '/^ *$/d' -e 's/^ *//' -e 's/[ \t][ \t]*/\t/g' -e 's/[ \t]*$//' results/standard.txt | tr 'a-z' 'A-Z' > results/file2
+  else
+    echo "Not supported"
+    exit 0
+  fi
   diff -u results/file1 results/file2
+  rm -f results/file* &>/dev/null
   echo "======================="
 done
 
