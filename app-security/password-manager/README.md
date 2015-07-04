572a295fe26145a0e47e0fd34abf889e56928deb
Robert Wen <robert.wen@nyu.edu>
2015-07-04 14:43:36 -0400

    argument parsing pass1

--- app-security/password-manager/README.md
+++ app-security/password-manager/README.md
@@ -4,7 +4,8 @@
  * Given a (Username, Password) pair in ASCII; check if the username exists and if the password matches the one stored in a file.
  * Using a flag the user should be able to choose ECB, CTR or CBC modes.
  * Program does not need to be interactive. Command line interface is sufficient.
- * Java has great crypto extensions and c/c++ has openssl, however feel free to use python or another scripting language. A master key needs to exist, could be auto generated and stored somewhere. Think about how shadow files work: If user1 and user2 have the same password, why do they show up differently? How can we make a bruteforcers' job harder if they had the masterkey?
+ * Java has great crypto extensions and c/c++ has openssl, however feel free to use python or another scripting language. 
+ * A master key needs to exist, could be auto generated and stored somewhere. 
 
 * Grading Criteria
 
@@ -19,6 +20,12 @@
 * Investigation
  * What is ECB, CTR or CBC? 
   * They are AES encryption mode.
+ * What is master key?
  * How to implemente them?
   * http://stackoverflow.com/questions/1220751/how-to-choose-an-aes-encryption-mode-cbc-ecb-ctr-ocb-cfb
   * http://docs.python-guide.org/en/latest/scenarios/crypto/
+
+* Write-up
+ * Think about how shadow files work: If user1 and user2 have the same password, why do they show up differently? 
+  * shadow file on Linux
+ * How can we make a bruteforcers' job harder if they had the masterkey?
--- app-security/password-manager/pwmgr.py
+++ app-security/password-manager/pwmgr.py
@@ -3,6 +3,14 @@
 """
 Password Manager
 
+Homework assignment for Application Security
+Summer 2015
+Polychenic School of Engineer, NYU
+
+Robert Wen <robert.wen@nyu.edu>
+NetID: qw476
+N12246277
+
 Example:
  ./pwmgr -a -u <username> -p <password>
  ./pwmgr -c -u <username> -p <password>
@@ -10,20 +18,50 @@ Example:
 """
 
 import os
+import sys
 import argparse
+
+class PasswordStore():
+  '''
+  '''
+
+class PasswordManager():
+  '''
+  class for global password manager
+  '''
+
+  ARGS = None
+
+  def __init__(self, argv):
+    self.ARGS = self.init_args(argv)
+
+  def init_args(self, argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--user', default=None, help='username')
+    parser.add_argument('--pass', default=None, help='password')
+    parser.add_argument('-a', nargs='?', help='add password into database')
+    parser.add_argument('-c', nargs='?', help='check password validity')
+    parser.add_argument('--type', default='ECB', help='cipher text type')
+
+    ARGS = parser.parse_args(argv)
+    return ARGS
+
+class Password():
+  '''
+  class for password entry
+  '''
+
+  username = None
+  password = None
+  passwd_cipher = None
+  
 
 def main():
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--user', default=None, help='username')
-  parser.add_argument('--pass1', default=None, help='password')
-  parser.add_argument('--add', help='add password into database')
-  parser.add_argument('--check', help='check password validity')
-  parser.add_argument('--type', default='ECB', help='cipher text type')
-
-  ARGS = parser.parse_args()
-
-  print ARGS.user, ARGS.pass1, ARGS.add
+  manager = PasswordManager(sys.argv[1:])
+  print manager.ARGS.user
+  print manager.ARGS.type
 
 if __name__ == '__main__':
   main()
