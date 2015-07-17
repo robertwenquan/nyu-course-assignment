e8a64eeabb8df64ce5e5946fe9c93490056127ef
Robert Wen <robert.wen@nyu.edu>
2015-07-17 11:43:52 -0400

    add placeholder for unittests

---
+++ app-security/password-manager/tests/test_pwmgr.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import unittest
+
+class TestPasswordMgr(unittest.TestCase):
+
+  def test_add_user(self):
+    print 'add user test'
+
+  def test_check_user(self):
+    print 'check user test'
+
+  def test_del_user(self):
+    print 'del user test'
+
+  def test_list_user(self):
+    print 'list user test'
+
+
+if __name__ == '__mani__':
+  unittest.main()
+
