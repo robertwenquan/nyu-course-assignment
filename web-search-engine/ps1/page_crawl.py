6a96dcb108e7dff29c258520a91a153e5c663411
Robert Wen <robert.wen@nyu.edu>
2015-09-11 21:23:16 -0400

    fake the page_crawl with 10 sub-pages per page

--- web-search-engine/ps1/page_crawl.py
+++ web-search-engine/ps1/page_crawl.py
@@ -16,6 +16,25 @@ class Page(object):
 class GenericPageCrawler(object):
   ''' Generic Web Page Crawler and Parser '''
 
-  def __init__(self, url):
-    self.url = url
+  def __init__(self, page, queue, cache):
+    self.url = page.url
+    self.depth = page.depth
+    self.score = page.score
+
+    self.queue = queue
+    self.cache = cache
+
+    self.parse()
+
+  def parse(self):
+    import random
+    import md5
+
+    for i in range(10):
+      m = md5.new()
+      m.update(str(random.randint(1000000, 6000000)))
+      url = 'http://www.fakeurl.com/%s' % m.hexdigest()
+
+      page = Page(url, self.depth + 1, random.randint(self.score - 40, self.score - 10))
+      self.queue.en_queue(page)
 
