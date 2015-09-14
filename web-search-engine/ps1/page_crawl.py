148097dfd20d2b639d513508f0dce0d81c6909ca
caicai <chencaicai1124@gmail.com>
2015-09-14 11:28:26 -0400

    count keyword in a page and update the page score

--- web-search-engine/ps1/page_crawl.py
+++ web-search-engine/ps1/page_crawl.py
@@ -23,7 +23,7 @@ class Page(object):
 class GenericPageCrawler(object):
   ''' Generic Web Page Crawler and Parser '''
 
-  def __init__(self, page, queue, cache):
+  def __init__(self, page, queue, cache, keywords):
     self.url = page.url
     self.depth = page.depth
     self.score = page.score
@@ -31,13 +31,26 @@ class GenericPageCrawler(object):
     self.queue = queue
     self.cache = cache
 
+    self.keywords = keywords
+
     self.parse()
+
+  def update_page_score(self, text):
+    keywords = self.keywords
+
+    for index, item in enumerate(keywords):
+      keywords[index] = keywords[index].lower()
+
+    words = text.split(" ")
+
+    for word in words:
+      if word.lower() in keywords:
+        self.score +=  10
 
   def parse(self):
     headers = {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
     }
-    black_list = ['mp3','mp4','pdf','doc','jpg','png','gif','exe','txt']
 
     '''get header of url to decide whether it is crawlable '''
     urlps = urlparse(self.url)
@@ -58,6 +71,14 @@ class GenericPageCrawler(object):
     data = response.text
     soup = BeautifulSoup(data,'html.parser')
 
+    ''' Update page score '''
+    self.update_page_score(soup.get_text())
+
+    '''Get and return links from current page'''
+    return self.get_next_level_page(soup)
+
+  def get_next_level_page(self, soup):
+    black_list = ['mp3','mp4','pdf','doc','jpg','png','gif','exe','txt']
     page_links = []
 
     for link in soup.find_all('a'):
@@ -75,8 +96,14 @@ class GenericPageCrawler(object):
 
       if extension in black_list:
         continue
-      page = Page(link, self.depth + 1, random.randint(self.score - 40, self.score - 10))
+
+      ''' CHECK DEDUPLICATION '''
+      if self.check_duplication(link):
+        continue
+
+      ''' If never visited, add to queue '''
+      page = Page(link, self.depth + 1, self.score/2)
       self.queue.en_queue(page)
 
-
-
+  def check_duplication(self, link):
+    return False
