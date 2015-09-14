ce431ab981ad57b9f72cbeec169dc248c9b5f2ff
Robert Wen <robert.wen@nyu.edu>
2015-09-14 11:00:08 -0400

    add two examples of c++

---
+++ programming-languages/c++/bool-array.cpp
@@ -0,0 +1,10 @@
+// my first program in C++
+#include <iostream>
+
+int main()
+{
+  bool digits_seen[10] = { false, true, true, false, true, false, false, false, false, false };
+
+  // read a number, dissect each digit and print only the numbers that were pre-set to be true
+}
+
