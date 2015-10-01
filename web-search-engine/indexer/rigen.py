41b753ba65d102c2bdc39f7a3ae50c788859debc
Robert Wen <robert.wen@nyu.edu>
2015-10-01 13:36:44 -0400

    add the starter kit for Python

---
+++ web-search-engine/indexer/rigen.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Reversed Index Generator
+
+parse a bunch of compressed web pages and create reserved index files
+"""
+
+__author__ = "Robert Wen <robert.wen@nyu.edu>, Caicai Chen <caicai.chen@nyu.edu>"
+
+
+def main():
+  ''' main routine '''
+  print "Let's build the Reversed Index in Python!"
+
+
+if __name__ == '__main__':
+  main()
+
