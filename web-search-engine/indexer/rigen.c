2ac8a68d558a11bbdf448e5df7688065b70d42c4
Robert Wen <robert.wen@nyu.edu>
2015-10-01 13:34:53 -0400

    add the C starter code for the rigen

---
+++ web-search-engine/indexer/rigen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ main function of the rigen (Reversed Index Generator)
+ */
+
+int main(int argc, char *argv[])
+{
+  printf("Let's build the Reversed Index in C!\n");
+  return 0;
+}
+
