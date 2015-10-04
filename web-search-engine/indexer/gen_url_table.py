3c391d47cd7ae0e332813e055c2c949606d0ab7e
Robert Wen <robert.wen@nyu.edu>
2015-10-04 03:39:05 -0400

    add lexicon analysis notebook

--- web-search-engine/indexer/gen_url_table.py
+++ web-search-engine/indexer/gen_url_table.py
@@ -35,6 +35,8 @@ WET_DIR = '/tmp/cc201507/'
 URL_TABLE_IDX = '/tmp/url_table.idx'
 URL_TABLE_URLS = '/tmp/url_table.urls'
 
+LEXICON_PATH = '/tmp'
+
 
 def get_wet_files():
   """ get a list of wet full path filenames """
@@ -47,6 +49,30 @@ def docid_generator():
   while True:
     docid += 1
     yield docid
+
+def wordid_generator():
+  """ generate wordid from 1 and upwards """
+  wordid = 0
+  while True:
+    wordid += 1
+    yield wordid
+
+def is_ascii(s):
+  return all(ord(c) < 128 for c in s)
+
+def split_with_offset(line, _len=len):
+  """ split string to tokens with offset """
+  words = line.split()
+  index = line.index
+  offsets = []
+  append = offsets.append
+  running_offset = 0
+  for word in words:
+    word_offset = index(word, running_offset)
+    word_len = _len(word)
+    running_offset = word_offset + word_len
+    append((word, word_offset, running_offset - 1))
+  return offsets
 
 
 class UrlIndex(object):
@@ -83,12 +109,16 @@ def main():
 
   wet_files = get_wet_files()
   docid_gen = docid_generator()
+  wordid_gen = wordid_generator()
 
   url_index = UrlIndex()
 
   for wet_file in wet_files:
     wet_fd = warc.open(wet_file)
     doc_next_offset = 0
+
+    lexicon_file = os.path.join(LEXICON_PATH, '.'.join(os.path.basename(wet_file).split('.')[:-1] + ['lexicon']))
+    lex_fd = open(lexicon_file, 'wb')
 
     for wet_record in wet_fd:
       if wet_record.url:
@@ -116,7 +146,20 @@ def main():
                         doc_offset, doc_length, content_offset, content_length)
         url_index.write_url_index_entry(url_idx_data)
 
+        # generate lexicons
+        saved_offset = wet_record.payload.fileobj.tell()
+        page_content = wet_record.payload.fileobj.read(content_length)
+        wet_record.payload.fileobj.seek(saved_offset)
+        for token, start, end in split_with_offset(page_content):
+          if is_ascii(token):
+            word_id = wordid_gen.next()
+            lexicon_data = pack('iiih', word_id, docid, start, 2)
+            lex_fd.write(lexicon_data)
+
         doc_next_offset = wet_record.payload.fileobj.tell() + wet_record.payload.length
+
+    lex_fd.close()
+    wet_fd.close()
 
 
 if __name__ == '__main__':
