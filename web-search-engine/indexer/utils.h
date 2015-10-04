dbce35eb6e6e41a4f7191c68018fa3366710d563
Robert Wen <robert.wen@nyu.edu>
2015-10-04 17:32:01 -0400

    add MIT_T and GIT_T

--- web-search-engine/indexer/utils.h
+++ web-search-engine/indexer/utils.h
@@ -3,5 +3,17 @@
 
 #include <stdio.h>
 
+typedef struct __attribute__((__packed__)) {
+  unsigned int word_id;
+  unsigned int offset;
+  unsigned short n_docs;
+} GIT_T;
+
+typedef struct __attribute__((__packed__)) {
+  unsigned int docid;
+  unsigned int offset;
+  unsigned short n_places;
+} MIT_T;
+
 #endif
 
