0facb92c7e630a834cb900885cb9b65d3751bef3
Robert Wen <robert.wen@nyu.edu>
2015-10-07 11:36:20 -0400

    add chk_lexicon script place holder

---
+++ web-search-engine/indexer/utils/chk_lexicons.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+check lexicons
+
+check the phase1 or phase2 output lexicon files
+for debugging purpose
+
+"""
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+  main()
+
