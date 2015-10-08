f1d70cc649a1bfdc7bdbf0d441c5967f41a55469
Robert Wen <robert.wen@nyu.edu>
2015-10-08 04:09:32 +0000

    fix a memory segfault bug

--- web-search-engine/indexer/lexicon.c
+++ web-search-engine/indexer/lexicon.c
@@ -24,13 +24,15 @@ char** get_lexicon_files()
 int lexicon_generator()
 {
   char **p = get_lexicon_files();
+  char **p_save = p;
+
   while (*p != NULL)
   {
     printf("%s\n", *p);
     p++;
   }
 
-  free(p);
+  free(p_save);
   return 0;
 }
 
@@ -96,6 +98,10 @@ int lexicon_sorter()
 {
   char *filename1 = "test_data/phase1_output/input1.warc.lexicon";
   char *filename2 = "test_data/bbb/input1.warc.lexicon";
+  /*
+  char *filename1 = "/data/wse/100k/phase1_output/CC-MAIN-20150728002301-00000-ip-10-236-191-2.ec2.internal.warc.lexicon";
+  char *filename2 = "/data/wse/100k/bbb.lexicon";
+  */
 
   int fd = open(filename1, O_RDWR);
   struct stat st;
