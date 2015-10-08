c9c4a2da62192db027fb0dae8be32e74f572e06f
Robert Wen <robert.wen@nyu.edu>
2015-10-08 10:26:46 -0400

    remove duplicated header files

--- web-search-engine/indexer/merge.c
+++ web-search-engine/indexer/merge.c
@@ -1,7 +1,3 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include"utils.h"
-
 /**************************************************************************/
 /* usage  ./merge maxdegree memsize finlist outfileprefix                 */
 /*                                                                        */
