bd27179937ccbd11bb7d4d17bc0c88c68d084cda
robertwenquan <robert.wen@nyu.edu>
2015-10-10 16:38:54 -0400

    add logger function

--- web-search-engine/indexer/utils.h
+++ web-search-engine/indexer/utils.h
@@ -2,6 +2,33 @@
 #define _iigen_utils_h
 
 #include <stdio.h>
+#include <time.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+// For logging
+#define LOG_DEBUG 0
+#define LOG_INFO  1
+#define LOG_WARN  2
+#define LOG_ERROR 3
+
+#define LOG_LEVEL_CHARS "DIWEF"
+#define LOG_MAX_MSG_LEN 1024
+
+typedef struct _logger {
+  int level;
+  char *datetime_format;
+  FILE *fp;
+} LOGGER_T;
+
+LOGGER_T * logger_create( void );
+void logger_free(LOGGER_T *l);
+void log_add(LOGGER_T *l, int level, const char *msg);
+void log_debug(LOGGER_T *l, const char *fmt, ...);
+void log_info(LOGGER_T *l, const char *fmt, ...);
+void log_warn(LOGGER_T *l, const char *fmt, ...);
+void log_error(LOGGER_T *l, const char *fmt, ...);
+// For logging
 
 // Global Index Table
 typedef struct __attribute__((__packed__)) {
