b7152812e42f9b435371d778e35e24e099e32cc0
Robert Wen <robert.wen@nyu.edu>
2015-10-12 15:26:40 +0000

    code refactor for iindex_gen

--- web-search-engine/indexer/iindex.c
+++ web-search-engine/indexer/iindex.c
@@ -17,15 +17,14 @@ GIT_T * cur_git;
 MIT_T * cur_mit;
 IIDX_T * cur_iidx;
 LEXICON_T * cur_lex;
-FILE *f_git = NULL, *f_mit = NULL, *f_iidx = NULL, *f_lex = NULL;
 
 static void print_help(char * argv[]);
 int initiate_global();
-void write_git(int count_in_git);
+void write_git(int count_in_git, FILE *fdw);
 void update_git(int word_id, int offset);
-void write_mit(int count_in_mit);
+void write_mit(int count_in_mit, FILE *fdw);
 void update_mit(int docid, int offset);
-void write_iidx(int offset);
+void write_iidx(int offset, FILE *fdw);
 
 static void print_help(char *argv[]) {
   printf("Help.\n");
@@ -63,7 +62,7 @@ int initiate_global(){
   return 0;
 }
 
-int gen_iindex()
+int gen_iindex(char *lex_file, char *git_file, char *mit_file, char *iidx_file)
 {
   int ret = 0;
   int count_in_git = 0;
@@ -71,30 +70,37 @@ int gen_iindex()
   int offset_in_git = 0;
   int offset_in_mit = 0;
 
+  FILE *f_git = NULL, *f_mit = NULL, *f_iidx = NULL, *f_lex = NULL;
+  f_lex = fopen(lex_file, "rb");
+  f_git = fopen(git_file, "wb");
+  f_mit = fopen(mit_file, "wb");
+  f_iidx = fopen(iidx_file, "wb");
+  assert(f_lex != NULL && f_git != NULL && f_mit != NULL && f_iidx != NULL);
+
   while (!feof(f_lex)) {
     ret = fread(cur_lex, sizeof(LEXICON_T), 1, f_lex);
     if (ret == 0) {
       // Write cur_mit, cur_git back;
-      write_git(count_in_git);
-      write_mit(count_in_mit);
+      write_git(count_in_git, f_git);
+      write_mit(count_in_mit, f_mit);
       break;
     }
 
     // SET OFFSET WHEN CREATE NEW GIT OR MIT, UPDATE COUNT WHEN WRITE BACK TO FILE
     if (cur_lex->word_id != cur_git->word_id) {
       //write cur_git to f_git, update with cur_lec
-      write_git(count_in_git);
+      write_git(count_in_git, f_git);
       update_git(cur_lex->word_id, offset_in_git);
       count_in_git = 0;
 
       //write cur_mit to f_mit, update with cur_lec
-      write_mit(count_in_mit);
+      write_mit(count_in_mit, f_mit);
       update_mit(cur_lex->docid, offset_in_mit);
       count_in_mit = 0;
       count_in_git++;
       offset_in_git++;
 
-      write_iidx(cur_lex->offset);
+      write_iidx(cur_lex->offset, f_iidx);
       count_in_mit++;
       offset_in_mit++;
  
@@ -105,13 +111,13 @@ int gen_iindex()
       //update count_in_git
 
       //write cur_mit to f_mit, update with cur_lec 
-      write_mit(count_in_mit);
+      write_mit(count_in_mit, f_mit);
       update_mit(cur_lex->docid, offset_in_mit);
       count_in_mit = 0;
       count_in_git++;
       offset_in_git++;
 
-      write_iidx(cur_lex->offset);
+      write_iidx(cur_lex->offset, f_iidx);
       count_in_mit++;
       offset_in_mit++;
 
@@ -123,20 +129,26 @@ int gen_iindex()
     }
 
     //update count_in_mit
-    write_iidx(cur_lex->offset);
+    write_iidx(cur_lex->offset, f_iidx);
     count_in_mit++;
     offset_in_mit++;
   }
+
+  fclose(f_iidx);
+  fclose(f_mit);
+  fclose(f_git);
+  fclose(f_lex);
+
   return 0;
 }
 
-void write_git(int count_in_git){
+void write_git(int count_in_git, FILE *fdw){
   if (count_in_git == 0 || cur_git->word_id == 0) {
     return;
   }
 
   cur_git->n_docs = count_in_git;
-  fwrite(cur_git, sizeof(GIT_T), 1, f_git);
+  fwrite(cur_git, sizeof(GIT_T), 1, fdw);
   return;
 }
 
@@ -147,13 +159,13 @@ void update_git(int word_id, int offset){
   return;
 }
 
-void write_mit(int count_in_mit){
+void write_mit(int count_in_mit, FILE *fdw){
   if (count_in_mit == 0 || cur_mit->docid == 0) {
     return;
   }
 
   cur_mit->n_places = count_in_mit;
-  fwrite(cur_mit, sizeof(MIT_T), 1, f_mit);
+  fwrite(cur_mit, sizeof(MIT_T), 1, fdw);
   return;
 }
 void update_mit(int docid, int offset){
@@ -163,99 +175,51 @@ void update_mit(int docid, int offset){
   return;
 }
 
-void write_iidx(int offset){
+void write_iidx(int offset, FILE *fdw){
   cur_iidx->offset = offset;
-  fwrite(cur_iidx, sizeof(LEXICON_T), 1, f_iidx);
+  fwrite(cur_iidx, sizeof(LEXICON_T), 1, fdw);
   return;
 }
 
-int index_builder(){
+int index_builder()
+{
   return 0;
 }
+
 int index_merger()
 {
   return 0;
 }
+
+
 #ifdef __TEST__
 int main(int argc, char * argv[])
 {
   int ret = 0;
   int numFile = 0;
-  char inputlist[1024] = {'\0'};
-  char filename[1024] = {'\0'};
   char outprefix[1024] = {'\0'};
-  FILE * fin;
-
-  if (argc != 3) {
-    print_help(argv);
-    return EXIT_FAILURE;
-  }
 
   ret = initiate_global();
   if (ret == -1) {
     return EXIT_FAILURE;
   }
 
-  if (argc != 3) {
-    print_help(argv);
-    return EXIT_FAILURE;
+  // get file list to process
+  char **p = get_inout_filelist(IINDEX_GENERATION);
+  char **p_save = p;
+
+  if (p == NULL) {
+    return 1;
   }
 
-  ret = initiate_global();
-  if (ret == -1) {
-    return EXIT_FAILURE;
-  }
-
-  strcpy(inputlist, argv[1]);
-
-  fin = fopen(inputlist, "r");
-  if (fin == NULL) {
-    printf("Open file %s failed", inputlist);
-    return EXIT_FAILURE;
-  }
-
-  while (!feof(fin)) {
-    ret = fscanf(fin, "%s", filename);
-    if(feof(fin)){
-      break;
-    }
-    if (ret == -1) {
-      return EXIT_FAILURE;
-    }
-
-    f_lex = fopen(filename, "r");
-    if (f_lex == NULL) {
-      return EXIT_FAILURE;
-    }
-
-    sprintf(outprefix, "%s%d", argv[2], numFile);
-
-    sprintf(filename, "%s%s", outprefix, ".git");
-    f_git = fopen(filename, "w");
-    if (f_git == NULL) {
-      return EXIT_FAILURE;
-    }
-
-    sprintf(filename, "%s%s", outprefix, ".mit");
-    f_mit = fopen(filename, "w");
-    if (f_mit == NULL) {
-      return EXIT_FAILURE;
-    }
-
-    sprintf(filename, "%s%s", outprefix, ".iidx");
-    f_iidx = fopen(filename, "w");
-    if (f_iidx == NULL) {
-      return EXIT_FAILURE;
-    }
-
-    gen_iindex();
-    fclose(f_iidx);
-    fclose(f_mit);
-    fclose(f_git);
-    fclose(f_lex);
-
+  while (*p != NULL && *(p+1) != NULL && *(p+2) != NULL && *(p+3) != NULL) {
+    gen_iindex(*p, *(p+1), *(p+2), *(p+3));
     numFile++;
+    p += 4;
   }
+
+  free_inout_filelist(p_save);
+
   return EXIT_SUCCESS;
 }
 #endif
