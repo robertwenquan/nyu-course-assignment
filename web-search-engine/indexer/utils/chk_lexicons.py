fbe971fa8f38ef8e798bc9572e5e139e5b65bddc
Robert Wen <robert.wen@nyu.edu>
2015-10-12 03:48:08 -0400

    add chk lexicon file script to validate lexicon files

--- web-search-engine/indexer/utils/chk_lexicons.py
+++ web-search-engine/indexer/utils/chk_lexicons.py
@@ -8,10 +8,36 @@ for debugging purpose
 
 """
 
-def main():
-  pass
+import os
+import sys
+from struct import unpack, calcsize
 
+
+def main(filename):
+  """ main routine """
+
+  lex_schema = 'iiih'
+
+  if not os.path.exists(filename):
+    print 'error'
+
+  offset = 0
+  rec_size = calcsize(lex_schema)
+
+  try:
+    fd = open(filename)
+    while True:
+      data = fd.read(rec_size)
+      if data == '':
+        break
+
+      aa = unpack(lex_schema, data)
+      print aa
+  except IOError:
+    # to handle the piped output to head
+    # like cmd | head
+    fd.close()
 
 if __name__ == '__main__':
-  main()
+  main(sys.argv[1])
 
