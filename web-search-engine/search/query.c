8a938e69a599002fca49c8e274bb93eb5d35b512
Robert Wen <robert.wen@nyu.edu>
2015-10-21 11:10:09 -0400

    generate some function stubs for word_id search

--- web-search-engine/search/query.c
+++ web-search-engine/search/query.c
@@ -8,6 +8,14 @@
 void query_word(char *word)
 {
   /* 1. word to word_id */
+  printf("query word_id for %s\n", word);
+  int word_id = -1;
+  word_id = word_to_id(word);
+  if (word_id < 0) {
+    return;
+  }
+
+  printf("word id: %d\n", word_id);
 
   /* 2. word_id to GIT entry */
 
@@ -34,6 +42,8 @@ void query_words(char *queries[])
 /* main routine */
 int main(int argc, char *argv[])
 {
+  query_word("fake");
+
   return 0;
 }
 
