21428146a4b548716077490d12f88d6b26fab0ed
Robert Wen <robert.wen@nyu.edu>
2015-10-21 11:22:17 -0400

    hook query_git and query_mit for testing

--- web-search-engine/search/query.c
+++ web-search-engine/search/query.c
@@ -18,10 +18,23 @@ void query_word(char *word)
   printf("word id: %d\n", word_id);
 
   /* 2. word_id to GIT entry */
+  printf("query GIT entry...\n");
+  GIT_T *p_git_entry = query_git(word_id);
+  if (p_git_entry == NULL) {
+    return;
+  }
+  print_git_entry(p_git_entry);
 
   /* 3. GIT entry to MIT entry */
+  printf("query MIT entry...\n");
+  MIT_T *p_mit_entry = query_mit(p_git_entry);
+  if (p_mit_entry == NULL) {
+    return;
+  }
+  print_mit_entry(p_mit_entry);
 
   /* 4. MIT entry to IINDEX entry */
+  printf("query IINDEX entry...\n");
 
   /* 5. IINDEX entry to WARC info */
 
