fbd723aa4c325cf3e4312628ba2257eab2631146
caicai <chencaicai1124@gmail.com>
2015-10-21 21:09:42 -0400

    fix bug of calloc lack of parameter

--- web-search-engine/search/iindex_search.c
+++ web-search-engine/search/iindex_search.c
@@ -114,7 +114,7 @@ MIT_T ** query_mit(GIT_T *p_git)
     return NULL;
   }
 
-  p_return_mit = (MIT_T **)calloc(sizeof(MIT_T *) * (p_git->n_docs + 1));
+  p_return_mit = (MIT_T **)calloc(p_git->n_docs + 1, sizeof(MIT_T *));
 
   MIT_T **p_head = p_return_mit;
 
