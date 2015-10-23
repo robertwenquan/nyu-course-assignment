b0f02a0c1caf937e4bd26635c66fd693b660c523
caicai <chencaicai1124@gmail.com>
2015-10-23 14:58:05 -0400

    Start compress index

---
+++ web-search-engine/indexer/compress.c
@@ -0,0 +1,33 @@
+#include "compress.h"
+
+void compress_iidx()
+{
+  /*
+   * Input : .mit file
+   * Output: compressed corresponding iidx file
+   *  1. Read each mit entry, update offset
+   *  2. Find corresponding iidx file, get all offset numbers
+   *  3. Call simple_9 to compress
+   *  4. Write back compressed numbers
+   *  5. Update global offset
+   */
+  
+  return;
+}
+
+void simple_9_compress()
+{
+  return;
+}
+
+void compress_mit()
+{
+  sort_mit();
+  return;
+}
+
+void sort_mit()
+{
+  return;
+}
+
