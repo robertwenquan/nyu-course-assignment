297a9f97147281e662e8fe4638a8f4726a1461e3
Robert Wen <robert.wen@nyu.edu>
2015-10-27 14:15:47 +0000

    page display

--- web-search-engine/search/searching/search/search.py
+++ web-search-engine/search/searching/search/search.py
@@ -9,12 +9,22 @@ def view(request):
       <TITLE>SEARCH ENGINE BY ROBERT and CAICAI</TITLE>
     </HEAD>
 
+    <BR>
+    <BR>
+    <BR>
+    <BR>
+    <BR>
+
     <BODY>
       <DIV ALIGN="CENTER">
       <IMG SRC="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" width="200" height="65">
+      <BR>
       <FORM action="/search" method="get">
         <DIV ALIGN="CENTER">
         <input style="padding: 0px; margin: 0px; height:30px; width: 60%; outline: none; background: url(data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw%3D%3D) transparent;" maxlength="256" name="q title="Search" type="text" value="">
+        <br>
+        <br>
+        <input type="submit" style="padding: 0px; margin: 0px; height:60px; width: 100px" value="Submit">
         </DIV>
       </FORM>
       </DIV>
